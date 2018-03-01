4478c4f13770acc471097edf663435029e09a7fe
MadsS <stoltenborg@hotmail.dk>
2018-03-01 12:01:30 +0100

    Flere ting

--- app/src/main/java/com/example/madsstoltenborg/miniprojekt/ProductListActvity.java
+++ app/src/main/java/com/example/madsstoltenborg/miniprojekt/ProductListActvity.java
@@ -1,13 +1,47 @@
 package com.example.madsstoltenborg.miniprojekt;
 
+import android.database.Cursor;
+import android.database.sqlite.SQLiteException;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
 public class ProductListActvity extends AppCompatActivity {
+    private Cursor cursor;
+    private SimpleCursorAdapter listAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_product_list_actvity);
+        ListView listDrinks = (ListView) findViewById(R.id.list_products);
+        try {
+            cursor = Storage.getInstance().getProducts();
+            listAdapter = new ShoppingCursorAdapter(this,
+                    R.layout.product_list_item,
+                    cursor,
+                    new String[]{"NAME"},
+                    new int[]{R.id.text},
+                    0);
+            listDrinks.setAdapter(listAdapter);
+        } catch (SQLiteException e) {
+            Toast toast = Toast.makeText(this, "Database unavailable", Toast.LENGTH_SHORT);
+            toast.show();
+        }
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        cursor = Storage.getInstance().getProducts();
+        listAdapter.changeCursor(cursor);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        cursor.close();
     }
 }
--- app/src/main/java/com/example/madsstoltenborg/miniprojekt/ShoppingCursorAdapter.java
+++ app/src/main/java/com/example/madsstoltenborg/miniprojekt/ShoppingCursorAdapter.java
@@ -1,8 +1,46 @@
 package com.example.madsstoltenborg.miniprojekt;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
+
 /**
  * Created by Mads Stoltenborg on 22-02-2018.
  */
 
-public class ShoppingCursorAdapter {
+public class ShoppingCursorAdapter extends SimpleCursorAdapter {
+    ShoppingCursorAdapter(Context context, int layout, Cursor c,
+                       String[] from, int[] to, int flags) {
+        super(context, layout, c, from, to, flags);
+    }
+
+    @Override
+    public void bindView(View view, final Context context, Cursor cursor) {
+        super.bindView(view, context, cursor);
+        CheckBox checkBox = (CheckBox) view.findViewById(R.id.checkbox);
+
+        /*checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                Toast.makeText(context,context.getString(R.string.checkbox_ischecked)
+                        + isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });*/
+
+        final int id = cursor.getInt(cursor.getColumnIndex("_id"));
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, R.string.item_clicked, Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(context, DrinkActivity.class);
+                intent.putExtra(DrinkActivity.EXTRA_DRINKID, (int) id);
+                context.startActivity(intent);
+            }
+        });
+    }
 }
